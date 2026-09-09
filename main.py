2a58a56b82d97995d8fefd13430826d9064357df
vzapatag5 <vzapatag5@eafit.edu.co>
2026-09-09 10:46:03 -0500

    error en test pytest

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 def saludo():
-    x = 123 #variable sin usar -> rompe pylint
     return "¡Hola CI/CD!"
--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
 from main import saludo 
 
 def test_saludo():
-    assert saludo() == "¡Hola CI/CD!"
+    assert saludo() == "Hola mundo" # para que falle pytest
